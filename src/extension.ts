ee91bca3738a98126cd3de78882d33662b494372
Kavin Muralikrishnan <mario.williams927@gmail.com>
2024-12-01 22:08:00 -0500

    Add javadoc string maker

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('javadoc-comment-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Javadoc Comment Generator!');
-	});
+	const disposable = vscode.commands.registerCommand('javadoc-comment-generator.helloWorld', () => {getMethods(vscode.window.activeTextEditor);});
 
 	context.subscriptions.push(disposable);
 }
@@ -25,6 +21,57 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-export function listSymbols() {
-	
+export async function getMethods(activeEditor: vscode.TextEditor | undefined) {
+	if(activeEditor){
+		let symbols: Array<vscode.DocumentSymbol> | undefined = await vscode.commands.executeCommand(
+			'vscode.executeDocumentSymbolProvider',
+			activeEditor.document.uri
+		);
+		if(symbols){
+			let methods: vscode.DocumentSymbol[] = [];
+			symbols.forEach((symbol) => addMethodsToArray(methods, symbol));
+			console.log(methods);
+			console.log("done");
+		}
+	}
 }
+export async function addMethodsToArray(methods: vscode.DocumentSymbol[], symbol: vscode.DocumentSymbol){
+	symbol.children.filter((child) => [vscode.SymbolKind.Function, vscode.SymbolKind.Method, vscode.SymbolKind.Constructor].includes(child.kind)).forEach((method) => methods.push(method));
+	symbol.children.filter((child) => [vscode.SymbolKind.Class, vscode.SymbolKind.Enum].includes(child.kind)).forEach((child) => addMethodsToArray(methods, child));
+}
+
+export async function handleMethods(activeEditor: vscode.TextEditor | undefined, methods: Array<vscode.DocumentSymbol>){
+	methods.forEach((method) => {
+		if(!activeEditor?.document?.getText(method.range).includes("/**")){
+			let params = [];
+			let returnVar = (!method.detail.includes("void"))? true: false;
+			if(!method.name.includes("()")){ //This is so janky
+				
+			}
+		}});
+}
+
+export function createJavaDocString(description:string, parameter:{string: string}, returnVar: string | undefined, deprecated: string | undefined): String{
+	let o="/**";
+	let splitDescription = [];
+	while(description.length>120){
+		splitDescription.push(description.substring(0, Math.min(description.length, 120)));
+		description=description.substring(Math.min(description.length, 120));
+	}
+	splitDescription.forEach((descriptionSection) => o+="\n * "+descriptionSection);
+	Object.entries(parameter).forEach(
+		([name, desc]) => o+=`\n * @param ${name} ${desc}`
+	);
+	if(returnVar) {o+=`\n * @return ${returnVar}`;}
+	if(deprecated) {o+=`\n * @deprecated Use {@link ${deprecated}} instead`;}
+
+	o+="\n */";
+	return "";
+}
+
+// export async function getClassVariables(params:type) {
+	
+// }
+/**
+ * 
+ */
